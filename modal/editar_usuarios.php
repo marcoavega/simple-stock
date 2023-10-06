42703e062bec30a73b47410831997c0bd3f1a919
marco <marcoavegagon@gmail.com>
2023-10-06 16:54:29 -0600

    6/10/23 4:54

--- modal/editar_usuarios.php
+++ modal/editar_usuarios.php
@@ -10,6 +10,7 @@
 			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 			<h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-edit'></i> Editar usuario</h4>
 		  </div>
+		  
 		  <div class="modal-body">
 			<form class="form-horizontal" method="post" id="editar_usuario" name="editar_usuario">
 			<div id="resultados_ajax2"></div>
@@ -22,10 +23,8 @@
 				  <input type="hidden" id="mod_id" name="mod_id">
 				</div>
 			  </div>
-			 
-						 	 
-			
 		  </div>
+
 		  <div class="modal-footer">
 			<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
 			<button type="submit" class="btn btn-primary" id="actualizar_datos">Actualizar datos</button>
