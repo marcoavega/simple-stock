b00673114d54f2761f7a803bad82d854eb08075f
marco <marcoavegagon@gmail.com>
2023-10-07 01:13:57 -0600

    agregar usuario nuevo con permisos

--- ajax/nuevo_usuario.php
+++ ajax/nuevo_usuario.php
@@ -1,96 +1,93 @@
 <?php
-include('is_logged.php');//Archivo verifica que el usario que intenta acceder a la URL esta logueado
-// checking for minimum PHP version
+include('is_logged.php'); // Archivo verifica que el usuario que intenta acceder a la URL esté logueado
+
+// Verificación de la versión mínima de PHP
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
     exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
-    // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
-    // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+    // Si estás usando PHP 5.3 o PHP 5.4, debes incluir password_api_compatibility_library.php
+    // (esta biblioteca agrega las funciones de hash de contraseña de PHP 5.5 a versiones más antiguas de PHP)
     require_once("../libraries/password_compatibility_library.php");
-}		
-		if (empty($_POST['user_password_new']) || empty($_POST['user_password_repeat'])) {
-            $errors[] = "Contraseña vacía";
-        } elseif ($_POST['user_password_new'] !== $_POST['user_password_repeat']) {
-            $errors[] = "la contraseña y la repetición de la contraseña no son lo mismo";
-        } elseif (strlen($_POST['user_password_new']) < 6) {
-            $errors[] = "La contraseña debe tener como mínimo 6 caracteres";
-        } elseif (strlen($_POST['user_name']) > 64 || strlen($_POST['user_name']) < 2) {
-            $errors[] = "Nombre de usuario no puede ser inferior a 2 o más de 64 caracteres";
-        } elseif (!preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])) {
-            $errors[] = "Nombre de usuario no encaja en el esquema de nombre: Sólo aZ y los números están permitidos , de 2 a 64 caracteres";
-        } elseif (
-			!empty($_POST['user_name'])
-            && strlen($_POST['user_name']) <= 64
-            && strlen($_POST['user_name']) >= 2
-            && preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])
-            && !empty($_POST['user_password_new'])
-            && !empty($_POST['user_password_repeat'])
-            && ($_POST['user_password_new'] === $_POST['user_password_repeat'])
-        ) {
-            require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
-			require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
-			
-				// escaping, additionally removing everything that could be (html/javascript-) code
-                
-				$user_name = mysqli_real_escape_string($con,(strip_tags($_POST["user_name"],ENT_QUOTES)));
-				$user_password = $_POST['user_password_new'];
-				$date_added=date("Y-m-d H:i:s");
-                // crypt the user's password with PHP 5.5's password_hash() function, results in a 60 character
-                // hash string. the PASSWORD_DEFAULT constant is defined by the PHP 5.5, or if you are using
-                // PHP 5.3/5.4, by the password hashing compatibility library
-				$user_password_hash = password_hash($user_password, PASSWORD_DEFAULT);
-					
-                // check if user or email address already exists
-                $sql = "SELECT * FROM users WHERE user_name = '" . $user_name . "' ;";
-                $query_check_user_name = mysqli_query($con,$sql);
-				$query_check_user=mysqli_num_rows($query_check_user_name);
-                if ($query_check_user == 1) {
-                    $errors[] = "Lo sentimos , el nombre de usuario  ya está en uso.";
-                } else {
-					// write new user's data into database
-                    $sql = "INSERT INTO users (user_name, user_password_hash, date_added)
-                            VALUES('" . $user_name . "', '" . $user_password_hash . "', '".$date_added."');";
-                    $query_new_user_insert = mysqli_query($con,$sql);
+}
 
-                    // if user has been added successfully
-                    if ($query_new_user_insert) {
-                        $messages[] = "La cuenta ha sido creada con éxito.";
-                    } else {
-                        $errors[] = "Lo sentimos , el registro falló. Por favor, regrese y vuelva a intentarlo.";
-                    }
-                }
-            
+if (empty($_POST['user_password_new']) || empty($_POST['user_password_repeat'])) {
+    $errors[] = "Contraseña vacía";
+} elseif ($_POST['user_password_new'] !== $_POST['user_password_repeat']) {
+    $errors[] = "La contraseña y la repetición de la contraseña no son iguales";
+} elseif (strlen($_POST['user_password_new']) < 6) {
+    $errors[] = "La contraseña debe tener al menos 6 caracteres";
+} elseif (strlen($_POST['user_name']) > 64 || strlen($_POST['user_name']) < 2) {
+    $errors[] = "El nombre de usuario no puede tener menos de 2 o más de 64 caracteres";
+} elseif (!preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])) {
+    $errors[] = "El nombre de usuario no encaja en el esquema de nombre: Solo se permiten letras y números, de 2 a 64 caracteres";
+} elseif (
+    !empty($_POST['user_name'])
+    && strlen($_POST['user_name']) <= 64
+    && strlen($_POST['user_name']) >= 2
+    && preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])
+    && !empty($_POST['user_password_new'])
+    && !empty($_POST['user_password_repeat'])
+    && ($_POST['user_password_new'] === $_POST['user_password_repeat'])
+) {
+    require_once("../config/db.php"); // Contiene las variables de configuración para conectar a la base de datos
+    require_once("../config/conexion.php"); // Contiene la función que conecta a la base de datos
+
+    // Escapando y eliminando cualquier cosa que pueda ser (html/javascript-) código
+    $user_name = mysqli_real_escape_string($con, (strip_tags($_POST["user_name"], ENT_QUOTES)));
+    $user_password = $_POST['user_password_new'];
+    $date_added = date("Y-m-d H:i:s");
+    $user_password_hash = password_hash($user_password, PASSWORD_DEFAULT);
+
+    // Recupera el id_permisos desde el formulario
+    $id_permisos = (int)$_POST['id_permisos'];
+
+    // Verifica si el usuario o la dirección de correo electrónico ya existen
+    $sql = "SELECT * FROM users WHERE user_name = '" . $user_name . "';";
+    $query_check_user_name = mysqli_query($con, $sql);
+    $query_check_user = mysqli_num_rows($query_check_user_name);
+    if ($query_check_user == 1) {
+        $errors[] = "Lo sentimos, el nombre de usuario ya está en uso.";
+    } else {
+        // Inserta los datos del nuevo usuario en la base de datos
+        $sql = "INSERT INTO users (user_name, user_password_hash, date_added, id_permisos)
+                VALUES('" . $user_name . "', '" . $user_password_hash . "', '" . $date_added . "', '" . $id_permisos . "');";
+        $query_new_user_insert = mysqli_query($con, $sql);
+
+        // Si el usuario se ha añadido con éxito
+        if ($query_new_user_insert) {
+            $messages[] = "La cuenta ha sido creada con éxito.";
         } else {
-            $errors[] = "Un error desconocido ocurrió.";
+            $errors[] = "Lo sentimos, el registro falló. Por favor, regrese e inténtelo de nuevo.";
         }
-		
-		if (isset($errors)){
-			
-			?>
-			<div class="alert alert-danger" role="alert">
-				<button type="button" class="close" data-dismiss="alert">&times;</button>
-					<strong>Error!</strong> 
-					<?php
-						foreach ($errors as $error) {
-								echo $error;
-							}
-						?>
-			</div>
-			<?php
-			}
-			if (isset($messages)){
-				
-				?>
-				<div class="alert alert-success" role="alert">
-						<button type="button" class="close" data-dismiss="alert">&times;</button>
-						<strong>¡Bien hecho!</strong>
-						<?php
-							foreach ($messages as $message) {
-									echo $message;
-								}
-							?>
-				</div>
-				<?php
-			}
+    }
+} else {
+    $errors[] = "Un error desconocido ha ocurrido.";
+}
 
+if (isset($errors)) {
+    ?>
+    <div class="alert alert-danger" role="alert">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>Error!</strong>
+        <?php
+        foreach ($errors as $error) {
+            echo $error;
+        }
+        ?>
+    </div>
+    <?php
+}
+if (isset($messages)) {
+    ?>
+    <div class="alert alert-success" role="alert">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>¡Bien hecho!</strong>
+        <?php
+        foreach ($messages as $message) {
+            echo $message;
+        }
+        ?>
+    </div>
+    <?php
+}
 ?>
--- modal/registro_usuarios.php
+++ modal/registro_usuarios.php
@@ -1,53 +1,77 @@
-	<?php
-		if (isset($con))
-		{
-	?>
-	<!-- Modal -->
-	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	  <div class="modal-dialog" role="document">
-		<div class="modal-content">
-		  <div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			<h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-edit'></i> Agregar nuevo usuario</h4>
-		  </div>
-		  <div class="modal-body">
-			<form class="form-horizontal" method="post" id="guardar_usuario" name="guardar_usuario">
-			<div id="resultados_ajax"></div>
-			  
-			  <div class="form-group">
-				<label for="user_name" class="col-sm-3 control-label">Usuario</label>
-				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="user_name" name="user_name" placeholder="Usuario" pattern="[a-zA-Z0-9]{2,64}" title="Nombre de usuario ( sólo letras y números, 2-64 caracteres)"required>
-				</div>
-			  </div>
-			  
-			  <div class="form-group">
-				<label for="user_password_new" class="col-sm-3 control-label">Contraseña</label>
-				<div class="col-sm-8">
-				  <input type="password" class="form-control" id="user_password_new" name="user_password_new" placeholder="Contraseña" pattern=".{6,}" title="Contraseña ( min . 6 caracteres)" required>
-				</div>
-			  </div>
-			  <div class="form-group">
-				<label for="user_password_repeat" class="col-sm-3 control-label">Repite contraseña</label>
-				<div class="col-sm-8">
-				  <input type="password" class="form-control" id="user_password_repeat" name="user_password_repeat" placeholder="Repite contraseña" pattern=".{6,}" required>
-				</div>
-			  </div>
-			 
-			  
+<?php
+if (isset($con)) {
+    ?>
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-edit'></i> Agregar nuevo usuario
+                    </h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal" method="post" id="guardar_usuario" name="guardar_usuario">
+                        <div id="resultados_ajax"></div>
 
-			 
-			 
-			
-		  </div>
-		  <div class="modal-footer">
-			<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-			<button type="submit" class="btn btn-primary" id="guardar_datos">Guardar datos</button>
-		  </div>
-		  </form>
-		</div>
-	  </div>
-	</div>
-	<?php
-		}
-	?>
+                        <div class="form-group">
+                            <label for="user_name" class="col-sm-3 control-label">Usuario</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" id="user_name" name="user_name"
+                                       placeholder="Usuario" pattern="[a-zA-Z0-9]{2,64}"
+                                       title="Nombre de usuario (sólo letras y números, 2-64 caracteres)" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="user_password_new" class="col-sm-3 control-label">Contraseña</label>
+                            <div class="col-sm-8">
+                                <input type="password" class="form-control" id="user_password_new"
+                                       name="user_password_new" placeholder="Contraseña" pattern=".{6,}"
+                                       title="Contraseña (min. 6 caracteres)" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="user_password_repeat" class="col-sm-3 control-label">Repite contraseña</label>
+                            <div class="col-sm-8">
+                                <input type="password" class="form-control" id="user_password_repeat"
+                                       name="user_password_repeat" placeholder="Repite contraseña" pattern=".{6,}"
+                                       required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="id_permisos" class="col-sm-3 control-label">Permiso de usuario</label>
+                            <div class="col-sm-8">
+                                <select class="form-control" id="id_permisos" name="id_permisos" required>
+                                    <option value="">Selecciona un permiso</option>
+                                    <?php
+                                    // Consulta SQL para obtener los permisos de la tabla permisos
+                                    $sql_permisos = "SELECT id_permisos, nombre_permiso FROM permisos";
+                                    $result_permisos = mysqli_query($con, $sql_permisos);
+
+                                    // Genera las opciones del campo select
+                                    while ($row_permiso = mysqli_fetch_assoc($result_permisos)) {
+                                        $id_permiso = $row_permiso['id_permisos'];
+                                        $nombre_permiso = $row_permiso['nombre_permiso'];
+                                        echo "<option value='$id_permiso'>$nombre_permiso</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                        <button type="submit" class="btn btn-primary" id="guardar_datos">Guardar datos</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <?php
+}
+?>
